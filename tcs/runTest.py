1a4f665ff1f0b3cac49d0f08f0d0d4cb29ec066d
rza <404261318@qq.com>
2019-06-01 22:09:26 +0800

    0601

--- tcs/runTest.py
+++ tcs/runTest.py
@@ -1,13 +1,16 @@
 # coding:utf-8
 import unittest
 import HTMLTestRunner
+import sys
+import os
 
 if __name__ == '__main__':
-    path = './'
-    file_path = 'd:/test.html'
-    ts = unittest.defaultTestLoader.discover(path, pattern='TC*.py')
+    fp = os.path.join(sys.argv[1], 'result.html')
+    tp = './'
+    # fp = 'd:/test.html'
+    ts = unittest.defaultTestLoader.discover(tp, pattern='TC*.py')
     # ts = unittest.defaultTestLoader.discover(path, pattern='select*.py')
-    f = open(file_path, mode='wb')
+    f = open(fp, mode='wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=f, title='测试报告', description='用例执行情况')
     runner.run(ts)
     f.close()
