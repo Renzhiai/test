b96cf82d36eb9e7aa29d0ca56acf1c526554b762
rza <404261318@qq.com>
2019-06-01 12:15:37 +0800

    0601

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env
 # coding:utf-8
 import unittest
+import HTMLTestRunner
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -14,4 +14,15 @@ class Test(unittest.TestCase):
         self.assertEqual(3, 1 + 2)
 
 if __name__ == '__main__':
-    unittest.main()
+    # 定义一个测试容器
+    test = unittest.TestSuite()
+    # 将测试用例，加入到测试容器中
+    test.addTest(Test('test_add'))
+    # 定义报告存放的路径，支持相对路径
+    file_path = "d:/result.html"
+    file_result = open(file_path, mode='wb')
+    # 定义测试报告
+    runner = HTMLTestRunner.HTMLTestRunner(stream=file_result, title='测试报告', description='用例执行情况')
+    # 运行测试用例
+    runner.run(test)
+    file_result.close()
