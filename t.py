cc4a9b51a532d89e620651570986b71700289492
rza <404261318@qq.com>
2019-06-01 11:05:44 +0800

    0601

--- t.py
+++ t.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env
-# coding = utf-8
-import os
+# coding:utf-8
+import unittest
 
-print('ttt')
+class Test(unittest.TestCase):
+    def setUp(self):
+        print('开始')
+
+    def tearDown(self):
+        print('结束')
+
+    def test_add(self):
+        print('test add')
+        self.assertEqual(3, 1 + 1)
+
+if __name__ == '__main__':
+    unittest.main()
