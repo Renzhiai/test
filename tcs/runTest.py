6a24169684890a76333d7667dc8772de83e53d21
rza <404261318@qq.com>
2019-06-01 22:21:56 +0800

    0601

--- tcs/runTest.py
+++ tcs/runTest.py
@@ -7,7 +7,7 @@ import os
 if __name__ == '__main__':
     tp = './'
     # fp = os.path.join(sys.argv[1], 'result.html')
-    fp = '/root/.jenkins/workspace/report/test.html'
+    fp = '/root/.jenkins/workspace/report/result.html'
     ts = unittest.defaultTestLoader.discover(tp, pattern='TC*.py')
     # ts = unittest.defaultTestLoader.discover(path, pattern='select*.py')
     f = open(fp, mode='wb')
