cfdf5ed81378ad3ccba7dbb6e7147f5c35abf025
rza <404261318@qq.com>
2019-06-01 22:37:09 +0800

    0601

--- tcs/TC001.py
+++ tcs/TC001.py
@@ -2,10 +2,13 @@
 # coding = utf-8
 import requests
 import unittest
-from .conf import *
+import configparser
 
 class SelectXmind(unittest.TestCase):
     def setUp(self):
+        conf = configparser.ConfigParser()
+        conf.read('conf.ini')
+        host = conf.get('conf', 'host')
         self.url = host + ':8001'
 
     def tearDown(self):
